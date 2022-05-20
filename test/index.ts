f6e944d61f9d68e5206a6405ef8ae285392ecb86
abdulrafaysiddiqui <abdul.rafay.siddique@gmail.com>
2022-05-20 18:04:41 +0500

    deployed version 0.63

--- test/index.ts
+++
@@ -1,5 +0,0 @@
-export { FarmsSdkProvider } from "../src/providers/farmsSdkProvider";
-export * from "../src/config/types";
-export * from "../src/hooks";
-export * from "../src/state/pools";
-export * from "../src/state/hooks";
